8b14ab849765d08076ad6d8055d49889dca2f186
yihhao wang <2633565580@qq.com>
2024-04-05 15:25:44 +0800

    docs(examples): update multi handlers examples

--- examples/multi_handler/main.go
+++ examples/multi_handler/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/getsentry/sentry-go"
 	"github.com/ginx-contribs/logx"
 	slogsentry "github.com/samber/slog-sentry/v2"
 	"log"
@@ -19,11 +18,11 @@ func main() {
 		log.Fatal(err)
 	}
 
-	sentryHandler := slogsentry.Option{Level: slog.LevelDebug}.NewSentryHandler()
-
 	logger, err := logx.New(
-		logx.WithHandlers(slog.NewJSONHandler(os.Stderr, nil)),
-		logx.WithHandlers(sentryHandler),
+		logx.WithHandlers(
+			slog.NewJSONHandler(os.Stderr, nil),
+			slogsentry.Option{Level: slog.LevelDebug}.NewSentryHandler(),
+		),
 	)
 	if err != nil {
 		log.Fatal(err)
